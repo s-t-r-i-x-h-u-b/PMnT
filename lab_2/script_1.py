58e97642b9a561ffae13f8dcfda0b9a60147abfa
aleksandr_filin <strixhub@gmail.com>
2025-11-06 18:39:41 +0300

    Сумма всех нечётных чисел до N

--- lab_2/script_1.py
+++ lab_2/script_1.py
@@ -1,5 +1,18 @@
-for i in range (2, 10):
-  for j in range (2, 10):
-    print (i, "*", j, "=", i * j)
-  print ("\n")
+def sum_odd_numbers(n):
+  sum = 0
+  for i in range (n + 1):
+    if i % 2:
+      sum += i
+  return sum
 
+def natural_number_input():
+  print("Введите число")
+  n = int(input())
+  while n < 1:
+    print("Число должно быть положительным")
+    n = int(input())
+  return n
+
+
+n = natural_number_input()
+print(f"Сумма всех нечётных чисел до {n} = {sum_odd_numbers(n)}")
