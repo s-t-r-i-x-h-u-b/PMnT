ce1ac4f0ee65374aeff826d54ea51fbc746304c4
aleksandr_filin <strixhub@gmail.com>
2025-11-06 18:41:13 +0300

    Сгенирировать список Фибоначчи

--- lab_2/script_4.py
+++ lab_2/script_4.py
@@ -1,20 +1,30 @@
-def nod (a, b):
-    if a < b:
-        temp = a
-        a = b
-        b = temp
-    while True:
-        if a == 0:
-            return b
-        if b == 0:
-            return a
-        temp = b
-        b = a % b
-        a = temp
-    
+def fibonacci(n):
+  l = []
+  a = 0
+  l.append(a)
+  if n == 1:
+    return l
+  b = 1
+  l.append(b)
+  if n == 2:
+    return l
+  n -= 2
+  while n > 0:
+    temp = b
+    b += a
+    a = temp
+    l.append(b)
+    n -= 1
+  return l
 
-print ("Введите первое число: ")
-a = int(input())
-print ("Введите второе число: ")
-b = int(input())
-print ("Наибольший общий делитель:", nod(a,b))
+def natural_number_input():
+  print("Введите число")
+  n = int(input())
+  while n < 1:
+    print("Число должно быть положительным")
+    n = int(input())
+  return n
+
+
+n = natural_number_input()
+print(fibonacci(n))
